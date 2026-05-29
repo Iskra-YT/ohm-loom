2b3bce7253e6684348e2604e666ede86a5741779
Szymon Izdebski <iskra42@proton.me>
2026-05-29 11:16:49 +0200

    feat: add clean button

--- js/toolbox.js
+++ js/toolbox.js
@@ -4,3 +4,9 @@ import { Draw, getPoint } from "./draw.js";
 document.querySelector("#battery").addEventListener("click", () => {
     Draw.append(new Battery(getPoint(5), getPoint(3)));
 });
+
+document.querySelector("#delete").addEventListener("click", () => {
+    for (const el of Draw.getList()) {
+        Draw.remove(el);
+    }
+});
